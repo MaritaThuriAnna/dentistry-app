461228e9fef22ee4dd36fb1aa91f58a79bb1fd71
Marita-Thuri Anna <maritathurianna@gmail.com>
2025-02-10 20:22:05 +0200

    navbar and patient list init

--- src/app/pages/doctor-dashboard/doctor-dashboard.component.ts
+++ src/app/pages/doctor-dashboard/doctor-dashboard.component.ts
@@ -1,10 +1,13 @@
 // doctor-dashboard.component.ts:
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { RouterOutlet } from '@angular/router';
+import { PatientListComponent } from "./patient-list/patient-list.component";
 
 @Component({
   selector: 'app-doctor-dashboard',
-  imports: [CommonModule],
+  imports: [CommonModule, NavbarComponent, RouterOutlet, PatientListComponent],
   templateUrl: './doctor-dashboard.component.html',
   styleUrl: './doctor-dashboard.component.css'
 })
