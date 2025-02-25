d563fff5713e24b78406536108a61d0e17259687
Marita-Thuri Anna <maritathurianna@gmail.com>
2025-02-25 17:15:05 +0200

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { HttpClientModule } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(HttpClientModule), // Add this to provide HttpClient
+    importProvidersFrom(HttpClientModule),
+    provideAnimationsAsync(),
   ],
 }).catch((err) => console.error(err));
